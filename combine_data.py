0bfe5334239e37cda7099c4c6a6b3d5c317245cc
g2malik <gagankumar2k@gmail.com>
2021-08-03 17:12:09 -0400

    Innit commit to combine data

---
+++ combine_data.py
@@ -0,0 +1,30 @@
+# --- 
+# aims: Combines all the data files into one file
+# modefication history: gmalik, August, 2021; 
+
+# --------------------------------
+# import libraries 
+
+import numpy as np
+import time
+import os
+import sys
+# --------------------------------
+
+data_types = ['gaussian', 'quadrant', 'spatial']
+start_time = 0
+no_files = 0
+for type in data_types:
+    path = r'''C:\Users\gagan\Documents\Work\Results\GMM Database'''
+    wfile = r'''C:\Users\gagan\Documents\Work\Results\GMM Database\%s.txt'''%(type)
+    w = open (wfile, mode = 'w')
+    
+    for x in range(1,4):
+        for z in range(1,53):
+            fname = path + '\%s\%s %d %d %d.dat'%(type, type, start_time, x, z)
+            f = open (fname, mode = 'r')
+            no_files+=1
+            for line in f:
+                w.write(line)
+        print("Done")
+    print(no_files)
